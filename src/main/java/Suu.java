2834ee451f3ea1d3c5f6bbf5e85b3770527fa108
ongrussell <ongrusselllq@hotmail.com>
2026-01-22 21:32:00 +0800

    Level-1: Echo user input and exit on bye

--- src/main/java/Suu.java
+++ src/main/java/Suu.java
@@ -1,13 +1,35 @@
+import java.util.Scanner;
+
 public class Suu {
     public static final String LINE = "_________________________________________";
 
     public static void main(String[] args) {
         String name = "Suu";
+        String input = "";
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println(LINE);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do  for you?");
         System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+
+        while(true){
+            input = sc.nextLine();
+
+            if(input.equals("bye")){
+                System.out.println(LINE);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println(LINE);
+            System.out.println(input);
+            System.out.println(LINE);
+        }
+
+        sc.close();
+
     }
 }

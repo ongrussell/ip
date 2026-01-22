c146fa919f64920bc5e78d13616e8029f8a9b426
ongrussell <ongrusselllq@hotmail.com>
2026-01-22 21:12:01 +0800

    Level-0: Suu greets and exits

---
+++ src/main/java/Suu.java
@@ -0,0 +1,13 @@
+public class Suu {
+    public static final String LINE = "_________________________________________";
+
+    public static void main(String[] args) {
+        String name = "Suu";
+        System.out.println(LINE);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do  for you?");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
